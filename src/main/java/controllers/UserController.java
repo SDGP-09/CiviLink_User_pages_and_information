ab706fb7f0e27ad2b82d81991503fdcc114a15aa
Gayathma2003 <gayathmab@gmail.com>
2025-02-07 18:01:24 +0530

    Update UserController.java

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import services.UserService;
 import dtos.UserDTO;
@@ -10,6 +11,7 @@ import dtos.UserDTO;
 public class UserController {
     private final UserService userService;
 
+    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -22,5 +24,25 @@ public class UserController {
     @GetMapping("/{id}")
     public UserDTO getUserProfile(@PathVariable Long id) {
         return userService.getUserById(id);
+    }
+
+    @PostMapping("/contractors")
+    public UserDTO addContractor(@RequestBody UserDTO userDTO) {
+        return userService.addContractor(userDTO);
+    }
+
+    @PostMapping("/consultants")
+    public UserDTO addConsultant(@RequestBody UserDTO userDTO) {
+        return userService.addConsultant(userDTO);
+    }
+
+    @PutMapping("/{id}")
+    public UserDTO updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
+        return userService.updateUser(id, userDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
     }
 }
