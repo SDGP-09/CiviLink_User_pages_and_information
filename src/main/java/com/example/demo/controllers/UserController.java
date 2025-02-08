a6ae066b4e1495de6ce3d2133677305dd5cf93a2
Gayathma2003 <gayathmab@gmail.com>
2025-02-08 16:20:59 +0530

    Updated file paths and files

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,10 +1,10 @@
-package controllers;
+package com.example.demo.controllers;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
-import services.UserService;
-import dtos.UserDTO;
+import com.example.demo.services.UserService;
+import com.example.demo.dtos.UserDTO;
 
 @RestController
 @RequestMapping("/api/users")
