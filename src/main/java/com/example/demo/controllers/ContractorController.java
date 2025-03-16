28f25305262f3b6dfb8f4e79ebebabd31c8ec30e
Gayathma2003 <gayathmab@gmail.com>
2025-03-16 12:05:00 +0530

    CiviLink-user-pages-and-information-version-12
    Contractor service debugged.

--- src/main/java/com/example/demo/controllers/ContractorController.java
+++ src/main/java/com/example/demo/controllers/ContractorController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controllers;
 
 import com.example.demo.dtos.ContractorDTO;
-import com.example.demo.services.ContractorService;
+import com.example.demo.services.impl.ContractorServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,22 +14,22 @@ import java.util.Optional;
 public class ContractorController {
 
     @Autowired
-    private ContractorService contractorService;
+    private ContractorServiceImpl contractorServiceImpl;
 
     @PostMapping
     public ResponseEntity<String> createContractor(@RequestBody ContractorDTO contractorDTO) {
-        contractorService.addContractor(contractorDTO);
+        contractorServiceImpl.addContractor(contractorDTO);
         return ResponseEntity.ok("Contractor Added Successfully");
     }
 
     @GetMapping("/search")
     public ResponseEntity<List<ContractorDTO>> searchContractors(@RequestParam String query) {
-        return ResponseEntity.ok(contractorService.searchContractors(query));
+        return ResponseEntity.ok(contractorServiceImpl.searchContractors(query));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ContractorDTO> getContractorProfile(@PathVariable Long id) {
-        Optional<ContractorDTO> contractor = contractorService.getContractorProfile(id);
+        Optional<ContractorDTO> contractor = contractorServiceImpl.getContractorProfile(id);
         return contractor.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
--- src/main/java/com/example/demo/services/ContractorService.java
+++ src/main/java/com/example/demo/services/ContractorService.java
@@ -1,53 +1,13 @@
 package com.example.demo.services;
 
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.entities.Contractor;
-import com.example.demo.repositories.ContractorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import com.example.demo.dtos.ContractorDTO;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class ContractorService {
+public interface ContractorService  {
 
-    @Autowired
-    private ContractorRepository contractorRepository;
-
-    public ContractorDTO addContractor(ContractorDTO contractorDTO) {
-        Contractor contractor = new Contractor();
-        contractor.setName(contractorDTO.getName());
-        contractor.setLocation(contractorDTO.getLocation());
-        contractor.setCompanyName(contractorDTO.getCompanyName());
-//        contractor.setRating(contractorDTO.getRating());
-        contractor.setProfilePicture(contractorDTO.getProfilePicture());
-
-        contractorRepository.save(contractor);
-        return contractorDTO;
-    }
-
-
-    public List<ContractorDTO> searchContractors(String query) {
-//        List<Contractor> contractors = contractorRepository.findByNameContainingIgnoreCaseOrLocationContainingIgnoreCase(query, query);
-//        return contractors.stream()
-//                .map(c -> new ContractorDTO(c.getName(), c.getLocation(), c.getCompanyName(), c.getRating(), c.getProfilePicture()))
-//                .toList();
-        return null;
-    }
-
-    public Optional<ContractorDTO> getContractorProfile(Long id) {
-//        Optional<Contractor> contractor = contractorRepository.findById(id);
-//        if (contractor.isEmpty()) {
-//            throw new ResourceNotFoundException("Contractor not found with id: " + id);
-//        }
-//        Contractor savedContractor = contractor.get();
-//        return contractor.map(c -> new ContractorDTO(c.getName(), c.getLocation(), c.getCompanyName(), c.getRating(), c.getProfilePicture()));
-        return null;
-    }
-
+    public ContractorDTO addContractor(ContractorDTO contractorDTO);
+    public List<ContractorDTO> searchContractors(String query);
+    public Optional<ContractorDTO> getContractorProfile(Long id);
 }
-
-
-
