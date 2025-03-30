c71f43ea4a54c2d412c1caffa07ef0c1fc4e46c0
Gayathma2003 <gayathmab@gmail.com>
2025-03-30 14:04:37 +0530

    Change cross origin
    Cross origin allowed for all for development purpose.

--- src/main/java/com/example/demo/controllers/ImagesController.java
+++ src/main/java/com/example/demo/controllers/ImagesController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/images")
+@CrossOrigin("*")
 public class ImagesController {
     @Autowired
     private ImageService imageService;
